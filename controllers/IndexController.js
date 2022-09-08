0010cca2a179747986c5c832ecabeb5a6e51f7fd
Alima07 <alex.lima_dev@outlook.com>
2022-09-08 16:32:51 -0300

    Aplicacao pratica das aulas

--- controllers/IndexController.js
+++ controllers/IndexController.js
@@ -1,14 +1,34 @@
-// Criacao objeto Literal para armazenar as funcoes de controller de cada endereco indicado
-// pelo roteador para controle.
+const dataBasePizzas = require('../database/database_pizzas.json');
 
+// Criacao objeto Literal para armazenar as funcoes de controller de cada endereco indicado pelo roteador para controle.
 const IndexController = {
 
     index: (req, res)=>{
-        res.render('index');
+        res.render('index.ejs', {dataBasePizzas});
     },
 
     search: (req, res)=>{
-        res.send(req.query);
+
+        //Levanta o trecho que está sendo buscado (req.query.find)
+        let termoBuscado = req.query.find;
+
+        //Filtra as pizzas para obter somente as pizzas com esse trecho levando em consideracao letras Maiusculas e Minusculas
+        let pizzasFiltrada = dataBasePizzas.filter(pizza => pizza.nome.toLowerCase().includes(termoBuscado.toLowerCase()));
+
+        //retorna a view index.ejs, passando para ela somente as pizzas filtradas
+        res.render('index.ejs', {dataBasePizzas: pizzasFiltrada});
+    },
+
+    viewPizzas: (req, res)=>{
+
+        // Armazena o Id da Pizza Selecionada
+        let id = req.params.id
+
+        // Localiza no banco de dados a Pizza selecionada atraves do seu ID
+        let pizzaSelecionada =  dataBasePizzas.find(pizzas => pizzas.id == id);
+
+        //Rederiza a View da Pizza encontrada para o cliente
+        res.render('viewpizzas.ejs', {dataBasePizzas: pizzaSelecionada});
     }
 }
 
--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -1,16 +1,17 @@
+// Bloco para uso de Rotas do projeto (Requisicao Controller e Indicacao Caminho)
+var IndexController = require('../controllers/IndexController');
+
 // 1. Requizicao do modulo NPM Express
 var express = require('express');
 
 // 2 Config. Modulo express para chamar a funcao roteador
 var router = express.Router();
 
-// Bloco para uso de Rotas do projeto (Requisicao Controller e Indicacao Caminho)
-var IndexController = require('../controllers/IndexController');
 router.get('/', IndexController.index);
 
 router.get('/search', IndexController.search);
 
-
+router.get('/pizzas/:id', IndexController.viewPizzas);
 
 //...................................
 
